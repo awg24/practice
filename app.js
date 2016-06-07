677573707f5faa60390a24c0186cfa2887185b96
Josh Madewell <josh.madewell@gmail.com>
2016-06-07 17:56:41 -0500

    changing some stuff

--- app.js
+++ app.js
@@ -39,11 +39,6 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'production') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
   });
 }
 
